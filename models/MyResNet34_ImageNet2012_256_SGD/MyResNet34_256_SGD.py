b364cf4ee06e44287e3b59b62926ba347377911c
JIHO_ubuntu <jiho264@naver.com>
2024-01-22 20:26:45 +0900

    make py file

---
+++ models/MyResNet34_ImageNet2012_256_SGD/MyResNet34_256_SGD.py
@@ -0,0 +1,268 @@
+# %% [markdown]
+# # Import
+
+# %%
+import torch
+from torch import nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau, MultiStepLR
+from fvcore.nn import FlopCountAnalysis, flop_count_table
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname("src"))))
+
+from src.Mydataloader import LoadDataset
+from src.Mymodel import MyResNet34
+from src.Mymodel import MyResNet_CIFAR
+from src.Mytraining import DoTraining
+from src.Earlystopper import EarlyStopper
+from src.LogViewer import LogViewer
+
+# %% [markdown]
+# # Setup
+
+# %%
+"""Dataset selection"""
+# DATASET = "CIFAR10"
+# DATASET = "CIFAR100"
+DATASET = "ImageNet2012"
+
+"""Model selection for CIFAR"""
+NUM_LAYERS_LEVEL = 5
+
+"""Dataset parameters"""
+BATCH = 256
+SHUFFLE = True
+NUMOFWORKERS = 8
+PIN_MEMORY = True
+SPLIT_RATIO = 0
+
+"""optimizer parameters"""
+OPTIMIZER = "SGD"
+# OPTIMIZER = "Adam"
+# OPTIMIZER = "Adam_decay"
+
+"""Learning rate scheduler parameters"""
+NUM_EPOCHS = 1000
+SCHEDULER_PARIENCE = 10
+COOLDOWN = 25
+
+"""Early stopping parameters"""
+EARLYSTOPPINGPATIENCE = 40
+file_path = ""
+if DATASET == "ImageNet2012":
+    file_path = f"MyResNet34_{BATCH}_{OPTIMIZER}"
+else:
+    file_path = f"MyResNet{NUM_LAYERS_LEVEL*6+2}_{BATCH}_{OPTIMIZER}"
+
+if SPLIT_RATIO != 0:
+    file_path += f"_{int(SPLIT_RATIO*100)}"
+
+# %%
+file_path
+
+# %% [markdown]
+# # Loading the dataset
+
+# %% [markdown]
+# ## Define Dateloader
+
+# %%
+tmp = LoadDataset(root="../../data", seceted_dataset=DATASET, split_ratio=SPLIT_RATIO)
+train_data, valid_data, test_data, COUNT_OF_CLASSES = tmp.Unpack()
+
+# %%
+train_dataloader, valid_dataloader, test_dataloader = tmp.get_dataloader(
+    batch_size=BATCH, shuffle=SHUFFLE, num_workers=NUMOFWORKERS, pin_memory=PIN_MEMORY
+)
+
+# %% [markdown]
+# # Define ResNet
+
+# %% [markdown]
+# ## Model Confirm
+
+# %%
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device
+
+# %%
+if DATASET == "CIFAR10" or DATASET == "CIFAR100":
+    """ResNet{20, 32, 44, 56, 110, 1202} for CIFAR"""
+    model = MyResNet_CIFAR(
+        num_classes=COUNT_OF_CLASSES,
+        num_layer_factor=NUM_LAYERS_LEVEL,
+        Downsample_option="A",
+    ).to(device)
+    print(f"ResNet-{5*6+2} for {DATASET} is loaded.")
+
+elif DATASET == "ImageNet2012":
+    """ResNet34 for ImageNet 2012"""
+    model = MyResNet34(num_classes=COUNT_OF_CLASSES, Downsample_option="B").to(device)
+    # model = models.resnet34(pretrained=True).to(device)
+    # model = models.resnet34(pretrained=False).to(device)
+    print(f"ResNet-34 for {DATASET} is loaded.")
+
+
+# %%
+# model.named_modules
+
+# %%
+# tmp_input = torch.rand(BATCH, 3, 32, 32).to(device)
+# flops = FlopCountAnalysis(model, tmp_input)
+# print(flop_count_table(flops))
+
+# %% [markdown]
+# # Define Training
+
+# %% [markdown]
+# ## (1) Define Criterion
+
+# %%
+criterion = nn.CrossEntropyLoss()
+
+# %% [markdown]
+# ## (2) Define Optimazer
+
+# %%
+if OPTIMIZER == "Adam":
+    optimizer = torch.optim.Adam(model.parameters())
+elif OPTIMIZER == "Adam_decay":
+    optimizer = torch.optim.Adam(model.parameters(), weight_decay=1e-4)
+elif OPTIMIZER == "SGD":
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001
+    )
+
+# %% [markdown]
+# ## (3) Define Early Stopping
+
+# %%
+earlystopper = EarlyStopper(
+    patience=EARLYSTOPPINGPATIENCE, model=model, file_path=file_path
+)
+
+# %% [markdown]
+# ## (4) Define Learning Rate schedualer
+
+# %%
+scheduler = ReduceLROnPlateau(
+    optimizer,
+    mode="min",
+    patience=SCHEDULER_PARIENCE,
+    factor=0.1,
+    verbose=True,
+    threshold=1e-4,
+    cooldown=COOLDOWN,
+)
+
+
+# %% [markdown]
+# ## (5) Define AMP scaler
+
+# %%
+scaler = torch.cuda.amp.GradScaler(enabled=True)
+
+# %% [markdown]
+# ## Load before process
+
+# %%
+if os.path.exists(file_path + ".pth.tar"):
+    # Read checkpoint as desired, e.g.,
+    checkpoint = torch.load(
+        file_path + ".pth.tar",
+        map_location=lambda storage, loc: storage.cuda(device),
+    )
+    model.load_state_dict(checkpoint["model"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    scaler.load_state_dict(checkpoint["scaler"])
+    scheduler.load_state_dict(checkpoint["scheduler"])
+    earlystopper.load_state_dict(checkpoint["earlystopper"])
+    logs = checkpoint["logs"]
+
+    print("Suceessfully loaded the All setting and Log file.")
+    print(file_path)
+    print(f"Current epoch is {len(logs['train_loss'])}")
+    print(f"Current learning rate: {optimizer.param_groups[0]['lr']}")
+else:
+    # Create a dictionary to store the variables
+    train_loss = []
+    train_acc = []
+    eval_loss = []
+    valid_acc = []
+    test_loss = []
+    test_acc = []
+    lr_log = []
+    logs = {
+        "train_loss": train_loss,
+        "train_acc": train_acc,
+        "valid_loss": eval_loss,
+        "valid_acc": valid_acc,
+        "test_loss": test_loss,
+        "test_acc": test_acc,
+        "lr_log": lr_log,
+    }
+    print("File does not exist. Created a new log.")
+
+# %%
+optimizer.param_groups[0]["lr"]
+
+# %%
+earlystopper.early_stop_counter
+
+# %% [markdown]
+# # [Training Loop]
+
+# %%
+if valid_data is None:
+    valid_dataloader = None
+if test_data is None:
+    test_dataloader = None
+
+# %%
+Training = DoTraining(
+    model=model,
+    criterion=criterion,
+    optimizer=optimizer,
+    scaler=scaler,
+    scheduler=scheduler,
+    earlystopper=earlystopper,
+    device=device,
+    logs=logs,
+    file_path=file_path,
+)
+pre_epochs = len(Training.logs["train_loss"])
+
+for epoch in range(NUM_EPOCHS):
+    now = epoch + 1 + pre_epochs
+    print(f"[Epoch {epoch+1+pre_epochs}/{NUM_EPOCHS}] :")
+
+    if DATASET == "ImageNet2012":
+        eval_loss = Training.SingleEpoch(train_dataloader, valid_dataloader)
+    else:
+        eval_loss = Training.SingleEpoch(
+            train_dataloader, valid_dataloader, test_dataloader
+        )
+
+    Training.Save()
+
+    if earlystopper.check(eval_loss) == True:
+        break
+
+    print("-" * 50)
+
+# %%
+view = LogViewer(logs)
+view.draw()
+
+# %%
+view.print_all()
+
+# %%
+# CHECK = 5410
+# logs["train_loss"] = logs["train_loss"][:CHECK]
+# logs["train_acc"] = logs["train_acc"][:CHECK]
+# logs["valid_loss"] = logs["valid_loss"][:CHECK]
+# logs["valid_acc"] = logs["valid_acc"][:CHECK]
+# logs["test_loss"] = logs["test_loss"][:CHECK]
+# logs["test_acc"] = logs["test_acc"][:CHECK]
+# model.load_state_dict(torch.load(f"models/{file_path}.pth"))

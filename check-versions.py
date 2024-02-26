b05a31e50c53243d01f9892be88b2b1ddb46e97b
lee-LAB <jiho264@naver.com>
2024-02-26 20:29:58 +0900

    .

--- check-versions.py
+++ check-versions.py
@@ -1,8 +1,10 @@
 import torch
 import sys 
-print("Python version:", sys.version)
+print("")
+print("Python version :", sys.version)
 print("torch version : ", torch.__version__)
 print("cuda available : ", torch.cuda.is_available())
 print(torch.cuda.get_device_name(0))
-print("cudnn ver",torch.backends.cudnn.version())
+print("cudnn ver : ",torch.backends.cudnn.version())
 print("cudnn enabled:", torch.backends.cudnn.enabled)
+print("")
